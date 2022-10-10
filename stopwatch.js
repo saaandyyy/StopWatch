34c1dd0b95330417ad30ae8bd3bbe2eb63db48af
saaandyyy <sandeep.kumar15012001@gmail.com>
2022-10-10 19:43:56 +0530

    add comment in code

--- stopwatch.js
+++ stopwatch.js
@@ -1,6 +1,7 @@
+/* selecting the Html element with the help of Selector */
 window.onload = function () {
-    var second = 00;
-    var milliSec = 00;
+    var second = 00;  // Assingning the variable second with the value 00
+    var milliSec = 00;  // Assingning the variable milliSec with the value 00
     var appendMilli = document.getElementById("milliSec");
     var appendSec = document.getElementById("second");
     var buttonStart = document.getElementById("button-start");
@@ -8,15 +9,18 @@ window.onload = function () {
     var buttonReset = document.getElementById("button-reset");
     var Interval;
 
+    /* Function for after clicking on Start button */
     buttonStart.onclick = function() {
         clearInterval(Interval);
         Interval = setInterval(startTimer, 10);
     }
 
+    /* Function for after clicking on Stop button */
     buttonStop.onclick = function() {
         clearInterval(Interval);
     }
 
+    /* Function for after clicking on Reset button */
     buttonReset.onclick = function() {
         clearInterval(Interval);
         milliSec = "00";
@@ -25,23 +29,24 @@ window.onload = function () {
         appendSec.innerHTML = second;
     }
 
+    /* Function for Timer */
     function startTimer () {
         milliSec++;
 
-        if(milliSec <= 9){
+        if(milliSec <= 9){  // When MilliSecond will show the number less or equal to 9
             appendMilli.innerHTML = "0" + milliSec;
         }
-        if(milliSec > 9){
+        if(milliSec > 9){  // When MilliSecond will show the number greater to 9
             appendMilli.innerHTML = milliSec;
         }
-        if(milliSec > 99){
+        if(milliSec > 99){  // When MilliSecond will show the number greater to 99
             console.log("second");
             second++;
             appendSec.innerHTML = "0" + second;
             milliSec = 0;
             appendMilli.innerHTML = "0" + 0;
         }
-        if(second > 9){
+        if(second > 9){  // When Second will show the number greater to 9
             appendSec.innerHTML = second;
         }
     }
